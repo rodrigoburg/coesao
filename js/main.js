bc2fc131667effadb79be08dea8f7efbd2037fcd
rodrigoburg <rodrigo.burgarelli1@gmail.com>
2015-03-31 01:27:58 -0300

    foto pra o fb

--- js/main.js
+++ js/main.js
@@ -279,7 +279,7 @@ d3.json(url, function(nations) {
 // Positions the dots based on data.
     function position(dot) {
         dot 
-            .transition().duration(120)
+            .transition().duration(100)
             .attr("cx", function(d) { return xScale(transScale( x(d) )) ; } )
             .attr("cy", function(d) { return yScale(y(d)); })
             .attr("r", function(d) { raio_grupo = correcao_grupos(); return Math.abs(radiusScale(radius(d)/raioScale(raio_grupo))); })
